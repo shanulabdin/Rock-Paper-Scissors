e55706f660125329b5a9145a5d09585c28fb035f
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-15 00:24:58 +0500

    Add files via upload

---
+++ script/2-rock-paper-scissors.js
@@ -0,0 +1,32 @@
+function computerMove(){
+  const randomNumber = Math.random();
+
+
+  if (randomNumber >= 0 && randomNumber < 1/3){
+    return '✊';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3){
+    return '✋';
+  } else if (randomNumber >= 2/3 && randomNumber < 1){
+    return '✌️';
+  }
+}
+
+function playGame(move){
+  const computer = computerMove();
+  let result = ''
+
+  if (computer === move){
+    result = 'Tie';
+  } else if (
+    (computer === '✊' && move === '✋') ||
+    (computer === '✋' && move === '✌️') ||
+    (computer === '✌️' && move === '✊')
+  ){
+    result = 'You Win';
+  } else {
+    result = 'You Lose';
+  }
+
+  document.getElementById("computerMoveEmoji").innerText = computer;
+  document.getElementById("results").innerText = result;
+}
